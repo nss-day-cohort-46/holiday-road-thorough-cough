765df5c99caaa7d4ba151fcfc5691135b0e6c677
Scott47 <scottsilver47@gmail.com>
2021-02-07 18:31:08 -0600

    no changes

--- scripts/weather/Weather.js
+++ scripts/weather/Weather.js
@@ -1,33 +1,34 @@
 
 export const FiveDayForecast = (dailyWeatherArray) => {
-    
+
     return dailyWeatherArray.map(day => {
         let date = new Date(
             day.dt * 1000
         ).toLocaleDateString("en-US")
         let icon = "";
-        let weather = day.weather[0].icon;
+        const weather = day.weather[0].icon;
         if (weather === "01d") {
-        icon = "🌞";
+            icon = "🌞";
         }
         if (weather === "02d" || weather === "03d") {
-        icon = "⛅️";
+            icon = "⛅️";
         }
         if (weather === "04d") {
-        icon = "☁️";
+            icon = "☁️";
         }
         if (weather === "09d" || weather === "10d") {
-        icon = "🌧";
+            icon = "🌧";
         }
         if (weather === "11d") {
-        icon = "⛈";
+            icon = "⛈";
         }
         if (weather === "13d") {
-        icon = "❄️";
+            icon = "❄️";
         }
         return `<div class="weatherCard">
         <p class="weatherForecast">${date}</p>
         <div class="weather-icon">${icon}</div>
+        <p class="weatherDesc">${day.weather[0].description}</p>
         <p class="weatherLow">Low: ${Math.round(day.temp.min)}˚</p>
         <p class="weatherHigh">High: ${Math.round(day.temp.max)}˚</p>
         <p class="weatherHigh">Feels Like: ${Math.round(day.feels_like.day)}˚</p>
